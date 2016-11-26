df8799eaa428669d1c64433b0d39fb472d9954d7
smnicolas <smnicolas@gmail.com>
2016-11-26 11:56:35 -0300

    subo

--- contenido/temas.sh
+++ contenido/temas.sh
@@ -9,21 +9,50 @@ BASEDIR="."
 printf "\n## Temas\n\n"
 
 
-for d in $(ls -d */)
-do
-    if [ "$(ls $d)" ]
-    then
-	title=$(head "$d"title)
-	printf "\n%s\n\n" "$title"
+# for dir in $(ls -d */)
+# do
+#     # Si no esta vacio
+#     if [ "$(ls $dir)" ]
+#     then
+# 	title=$(head "$dir"title)
+# 	printf "\n%s\n\n" "$title"
 
-	for md in $(ls "$d"*.md)
-	do
-	    f=${md/*\//}
-	    fn=${f/.md/}
+# 	for md in $(ls "$dir"*.md)
+# 	do
+# 	    f=${md/*\//}
+# 	    fn=${f/.md/}
 
-	    link_title=$(head -1 $md | sed "s/## //")
-	    printf "* [%s](%s)\n" "${link_title}" $BASEDIR/$d$fn.html
-	done
+# 	    link_title=$(head -1 $md | sed "s/## //")
+# 	    printf "* [%s](%s)\n" "${link_title}" $BASEDIR/$dir$fn.html
+# 	done
 		
-    fi
-done
+#     fi
+# done
+
+function temas_subtemas
+{
+    # Cada subdirectorio es un tema
+    for dir in $(ls -d */)
+    do
+	# Si no esta vacio
+	if [ "$(ls $dir)" ]
+	then
+	    title=$(head "$dir"title)
+	    printf "\n%s\n\n" "$title"
+
+	    for md in $(ls "$dir"*.md)
+	    do
+		f=${md/*\//}
+		fn=${f/.md/}
+
+		link_title=$(head -1 $md | sed "s/## //")
+		printf "* [%s](%s)\n" "${link_title}" $BASEDIR/$dir$fn.html
+	    done
+
+	fi
+    done
+
+
+}
+
+temas_subtemas
